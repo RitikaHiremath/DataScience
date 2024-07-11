f2e626c020502fb5eb210ea1a5c16c4849549900
LeiSaar <s8lehuan@stud.uni-saarland.de>
2024-07-11 22:51:19 +0200

    Update LiveDashboardNormalized.py

--- src/DashBoards/LiveDashboardNormalized.py
+++ src/DashBoards/LiveDashboardNormalized.py
@@ -131,8 +131,8 @@ def col_rename(dataset):
 
 
 # Define paths to data, please use your own path
-ok_data_path = 'C:/Users/lukas/Documents/Studium/Courses Master/DataScience/InputData/OK_Measurements'
-nok_data_path = 'C:/Users/lukas/Documents/Studium/Courses Master/DataScience/InputData/NOK_Measurements'
+ok_data_path = ''
+nok_data_path = ''
 
 # Preprocess data
 all_100KHzdata, all_2000KHzdata = preprocess_data(ok_data_path, nok_data_path)
